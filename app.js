ccd7cc365d27d33a3066795e7b8bbb3bb9d41bca
Javibeth <162606941+Javibeth@users.noreply.github.com>
2024-04-26 23:20:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -151,7 +151,7 @@ function updateCommand() {
         articulosDeArresto.push(modifiedText);
     });
 
-    commandText = `/multas poner usuario: razon:\n` ;
+    commandText = `/multas poner usuario: razon:\nTotal: ${totalSanction} €` + commandText.substr(5);
     commandElem.textContent = quitarCoso(removePlusMinusFromText(commandText));
 
     if (totalSinPrefijo > 1000 || totalConPrefijo > 2000) {
